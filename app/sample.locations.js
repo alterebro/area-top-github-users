6042536ba2fc50d77aa69e992bb6557ee50dcc22
Jorge Moreno <alterebro@gmail.com>
2016-08-28 17:51:27 +0200

    locations.js sample file

---
+++ app/sample.locations.js
@@ -0,0 +1,30 @@
+[
+
+    {
+        "location" : "pays-basque",
+        "label" : "Pays Basque Français",
+        "search" : {
+            "include" : ["bayonne", "biarritz", "anglet", "saint-jean-de-luz", "hendaye", "saint-jean-pied-de-port", "mauleon"],
+            "exclude" : [],
+            "users_out" : [],
+            "min_repos" : 0,
+            "min_followers" : -1,
+            "max_pages" : 5,
+            "max_users" : 50
+        }
+    },
+	{
+        "location" : "euskadi",
+        "label" : "Euskadi",
+        "search" : {
+            "include" : ["bilbao", "bilbo", "vizcaya", "bizkaia", "san-sebastian", "donosti", "guipuzcoa", "gipuzkoa", "vitoria", "gasteiz", "alava", "araba"],
+			"exclude" : [],
+			"users_out" : [],
+            "min_repos" : 0,
+            "min_followers" : -1,
+            "max_pages" : 5,
+            "max_users" : 50
+        }
+    }
+
+]
