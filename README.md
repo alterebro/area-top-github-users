06b7ea35f4e4bc09869d82d4c28daa322efa0292
Jorge Moreno <alterebro@gmail.com>
2016-09-03 17:34:29 +0200

    js crawler codebase & restructure

--- README.md
+++ README.md
@@ -1,13 +1,50 @@
 ## Top GitHub users from a geographical area population.
 
+*( Unfinished - Work in progress )*
+
 Top GitHub users rank generator script for a given geographical area population query. [alterebro.github.io/area-top-github-users](https://alterebro.github.io/area-top-github-users/)
 
-*( Work in progress )*
 
-```sh
-# 1. Install dependencies
-$ sudo npm install
+1. Install dependencies
 
-# 2. Execute main script (app/app.js)
-$ npm start
-```
+	```sh
+	$ sudo npm install
+	```
+
+
+2. Rename `/app/github.sample.js` to `/app/github.js` with your own ID and Secret strings, you can get them by creating a new GitHub application here: [Register a new OAuth application](https://github.com/settings/applications/new)
+
+	```javascript
+	exports.github = {
+	    'id' : 'alphanumeric_string',
+	    'secret' : 'alphanumeric_string'
+	}
+	```
+
+3. Rename `/app/sample.locations.json` to `/app/locations.json` and edit the file, create as many location items as needed following the same JSON structure as shown below:
+
+	```javascript
+	[
+	    {
+	        "location" : "aquitaine", // main query string
+	        "label" : "Aquitaine",
+	        "search" : {
+	            "include" : ["bordeaux", "pau", "merignac", "pessac", "bayonne", "anglet", "bergerac", "biarritz"], // Array of locations to include on the search query
+				"exclude" : ["paris", "montpellier"], // Array of locations to exclude from the search query
+				"users_out" : ["username"], // Array of users banned by login name.			
+	            "min_repos" : 0, // minimum repositories. num value HIGHER THAN (>)
+	            "min_followers" : -1,  // minimum followers. num value HIGHER THAN (>)
+	            "max_pages" : 5, // maximum pages to crawl from github (max. 10)
+	            "max_users" : 200 // Maximum num of users to be stored.
+	        }
+	    }
+
+		// .. add more items
+	]
+	```
+
+4. Execute main script (app/main.js)
+
+	```sh
+	$ npm start
+	```
---
+++ app/main.js
@@ -0,0 +1,284 @@
+var fs = require("fs");
+var request = require("superagent");
+var cheerio = require("cheerio");
+var junk = require("junk");
+var github = require('./github');
+
+var GITHUB_ID = github.github.id;
+var GITHUB_SECRET = github.github.secret;
+
+
+// --------------------
+// Utils --------------
+function date_id() {
+
+    function pad (num, size) {
+        var s = num + '';
+        while (s.length < size) { s = '0' + s; }
+        return s;
+    }
+    function y(d) { return String( d.getFullYear() ).slice(-2); }
+    function m(d) { return pad( d.getMonth() + 1, 2); }
+    function d(d) { return pad( d.getDate(), 2); }
+
+    var time = new Date();
+    var output = y(time) + '' + m(time) + '' + d(time);
+    return output;
+}
+
+
+// ==============================================
+// ==============================================
+
+
+// ----------------------
+// Scraper --------------
+function get_users( location_config ) {
+
+    // There's a limit of 1K first matches on GitHub (10pages of 100)
+    location_config['search']['max_pages'] = ( location_config['search']['max_pages'] > 10 ) ? 10 : location_config['search']['max_pages'];
+
+    var base_url = 'https://api.github.com/search/users';
+        base_url += '?client_id=' + GITHUB_ID;
+        base_url += '&client_secret=' + GITHUB_SECRET;
+        base_url += '&q=type:user';
+        base_url += '+location:' + location_config['location']
+        base_url += '+repos:%3E' + location_config['search']['min_repos']
+        base_url += '+followers:%3E' + location_config['search']['min_followers']
+        base_url += '+sort:repositories';
+
+        var include_search = location_config['search']['include'];
+        for (var i = 0; i < include_search.length; i++) {
+            base_url += '+location:' + encodeURI(include_search[i]);
+        }
+        base_url += '&per_page=100';
+        base_url += '&page=';
+
+        var users = [];
+        var requested_pages = 0;
+        for (var i = 1; i <= location_config['search']['max_pages']; i++) {
+
+            request
+                .get( base_url + i )
+                .set( 'User-Agent', 'curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8r zlib/1.2.5' )
+                .end( function(err, res) {
+
+                    if ( err ) {
+                        console.log( ' (!) - Error:' + err.status + ' > ' + err.message );
+                    }
+
+                    // console.log( ' - Requesting : ' + res.req.path );
+                    // console.log( ' - Requesting : ' + res.req.path.substr( res.req.path.lastIndexOf('&')+1 ) );
+                    process.stdout.write( ' - Requesting : ' + res.req.path.substr( res.req.path.lastIndexOf('&')+1 ) + " \r" );
+
+                    requested_pages++;
+                    users = users.concat(res.body.items);
+                    if ( requested_pages >= location_config['search']['max_pages'] ) {
+                        console.log( ' - User search completed' )
+                        getting_users_done();
+                    }
+                });
+        }
+
+    function getting_users_done() {
+        var users_output = [];
+        for (var i = 0; i < users.length; i++) {
+
+            var user = {
+                "id" : users[i]['id'],
+                "login" : users[i]['login'],
+                "avatar" : users[i]['avatar_url'],
+                "url" : users[i]['html_url']
+            }
+
+            users_output.push(user);
+        }
+
+        the_users = users_output;
+
+        console.log( ' - get_users() function completed.' );
+        console.log( ' - Total users found : ' + the_users.length );
+
+        // GO TO THE NEXT STEP...
+        get_user_details();
+    }
+}
+
+
+function get_user_details() {
+
+    var all_the_users = [];
+
+    function get_details( user ) {
+
+        // process.stdout.write( " - requesting user : " + user['login'] + " ( " + user['url'] + " ). \r" );
+        request
+            .get(user['url'])
+            .end(function(err, res){
+
+                // Defaults everything to false
+                user['name']            = false;
+                user['company']         = false;
+                user['location']        = false;
+                user['blog']            = false;
+                user['bio']             = false;
+                user['followers']       = false;
+                user['following']       = false;
+                user['contributions']   = false;
+                // user['languages']    = false;
+
+                if (res) {
+
+                    var $ = cheerio.load( res.text );
+                    user['name'] = $('h1 div.vcard-fullname').text();
+                    user['company'] = $('ul.vcard-details li[itemprop="worksFor"]').text();
+                    user['location'] = $('ul.vcard-details li[itemprop="homeLocation"]').text();
+                    user['blog'] = $('ul.vcard-details li[itemprop="url"] a').attr('href') || ''; // TODO: check if it is a valid URL
+                    user['bio'] = $('div.user-profile-bio').text();
+                    user['followers'] = parseInt($('div.vcard-stats a.vcard-stat:nth-child(1) strong.vcard-stat-count').text().trim());
+                    user['following'] = parseInt($('div.vcard-stats a.vcard-stat:nth-child(3) strong.vcard-stat-count').text().trim());
+
+                    var contributions = $('div.flush:nth-child(2) h3').text().trim();
+                        contributions = contributions.split(' ')[0];
+                        contributions = parseInt(contributions.replace(',', ''));
+                    user['contributions'] = contributions;
+
+                } else {
+
+                    // Rubbish
+                    // error_users.push(user);
+                    error_users.push(user['login']);
+                }
+
+                // exclude locations
+                var exclude_user_by_location = false;
+                var exclude = current_config['search']['exclude'] || false;
+                if ( exclude ) {
+                    for (var i=0; i<exclude.length; i++) {
+                        var loc_lc = user['location'].toString().toLowerCase();
+                        if ( loc_lc.search(exclude[i]) > -1) {
+                            console.log( ' - (!) User ' + user['login'] + ' excluded by location.' );
+                            exclude_user_by_location = true;
+                            break;
+                        }
+                    }
+                }
+
+                // Exclude by login name
+                var exclude_user_by_loginname = false;
+                var banned = current_config['search']['users_out'] || false;
+                if ( banned ) {
+                    for (var i=0; i<banned.length; i++) {
+                        if ( user['login'].toString().toLowerCase().search(banned[i]) > -1) {
+                            console.log( ' - (!) User ' + user['login'] + ' excluded by login name.' );
+                            exclude_user_by_loginname = true;
+                            break;
+                        }
+                    }
+                }
+
+
+                if ( !exclude_user_by_location && !exclude_user_by_loginname ) {
+
+                    all_the_users.push(user);
+                }
+                requested_users++;
+
+                process.stdout.write(" - User data collecting progress > " + requested_users + " / " + users_num + " \r");
+                // console.log( ' - User : ' + user['login'] + ' data taken. ( ' + requested_users + '/' + users_num + ' )' );
+                if ( requested_users >= users_num  ) {
+                    console.log( ' - User data collecting progress > completed > ' + requested_users + ' users.' );
+
+                    // MOVE TO THE NEXT STEP...
+                    the_users = all_the_users;
+                    export_data();
+                }
+
+            });
+    }
+
+    // Iterate over all the users...
+    var users_num = the_users.length;
+    var requested_users = 0;
+    console.log( " - Requesting users details." );
+    console.log( ' - Users num: ' + users_num );
+
+    for (var i=0; i<users_num; i++) {
+        get_details( the_users[i] );
+    }
+}
+
+
+function export_data() {
+    console.log(
+        ' - Exporting ... trim users: max/parsed',
+        current_config['search']['max_users'] + '/' + the_users.length
+    );
+
+    // Sort by contributions descending
+    the_users.sort(function(a, b) {
+        return b['contributions'] - a['contributions'];
+    });
+
+    // Slice the array to the maximum number
+    the_users = the_users.slice(0, current_config['search']['max_users']);
+
+    // Export data
+    var output_filename = target_folder + current_config['location'] + '.json';
+    console.log( ' - Writing output on file : ' + output_filename );
+
+    fs.writeFileSync( output_filename, JSON.stringify(the_users), 'utf-8');
+    console.log( ' ! DONE ' );
+    console.log( ' ! error users: (?) ', error_users );
+    console.log( ' ! error users: (#) ', error_users.length );
+
+    // DONE. Go to the next item if apply
+    iterator++;
+    if ( iterator < config.length ) { run(); }
+}
+
+// ==============================================
+// ==============================================
+
+// -----------------
+// Init
+function run() {
+
+
+    // Reset all users data
+    error_users = [];
+    the_users = [];
+    current_config = config[iterator];
+
+    // msg
+    console.log( ' --- run() : ' + current_config['location'] + ' : ' + current_config['label']  + ' --- ' );
+
+    // ------
+    var target_file = target_folder + current_config['location'] + '.json';
+    if ( fs.existsSync(target_file) && skip_if_exists ) {
+
+        // SKIP.
+        iterator++;
+        if ( iterator < config.length ) { run(); }
+
+    } else {
+
+        // go!
+        get_users( current_config );
+
+    }
+}
+
+var error_users = [];
+var the_users = [];
+var iterator = 0;
+var skip_if_exists = false;
+var target_folder = './data/' + date_id() + '/';
+    if (!fs.existsSync( target_folder )){ fs.mkdirSync( target_folder ); }
+
+var config = fs.readFileSync('./app/locations.json');
+    config = JSON.parse(config);
+
+var current_config = config[iterator];
+
+    run();
