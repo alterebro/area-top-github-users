0381e11ed66b3526d49c60fd210390229afb0ceb
Jorge Moreno <alterebro@gmail.com>
2016-09-08 21:11:55 +0200

    error data.json file load fallback

--- frontend/_src.app.js
+++ frontend/_src.app.js
@@ -2,6 +2,8 @@
 var data_file = 'data/data.json';
 
 var vd = {
+	data_error_loading : null,
+
 	data_folders : null,
 	data_locations : null,
 	data_filedata : null,
@@ -23,20 +25,28 @@ var vm = new Vue({
 			var xhr = new XMLHttpRequest()
 				xhr.overrideMimeType("application/json");
 				xhr.open('GET', data_file, true);
-				xhr.onload = function () {
+				xhr.onreadystatechange = function() {
+					if (this.readyState === 4) {
+						if (this.status >= 200 && this.status < 400) {
+							// Success
+							var res = JSON.parse(this.responseText);
+							self.data_folders = res.folders;
+							self.data_locations = res.locations;
+							self.data_filedata = 'data/' + res.folders[0] + '/' + res.locations[0];
+							self.getItems();
+							self.data_error_loading = false;
 
-					var res = JSON.parse(this.responseText);
-					self.data_folders = res.folders;
-					self.data_locations = res.locations;
-					self.data_filedata = 'data/' + res.folders[0] + '/' + res.locations[0];
-					self.getItems();
-				}
+						} else {
+							// Error
+							self.data_error_loading = true;
+						}
+					}
+				};
+
 				xhr.send();
 				xhr = null;
 		},
 		getItems: function() {
-
-			console.log(this.data_filedata);
 
 			var self = this;
 			var xhr = new XMLHttpRequest();
