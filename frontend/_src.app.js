5bfe1fc42d002cdbd82ca26d5363b86d83ddc16f
Jorge Moreno <alterebro@gmail.com>
2016-09-08 13:56:22 +0200

    first working demo

--- frontend/_src.app.js
+++ frontend/_src.app.js
@@ -1,1 +1,56 @@
-console.log('this is app.js (_src)');
+
+var data_file = 'data/data.json';
+
+var vd = {
+	data_folders : null,
+	data_locations : null,
+	data_filedata : null,
+	data_items : null
+};
+
+var vm = new Vue({
+
+	el: '#app',
+	data: vd,
+	created: function () {
+		this.getDataFile();
+    },
+
+	methods: {
+		getDataFile: function () {
+
+			var self = this
+			var xhr = new XMLHttpRequest()
+				xhr.overrideMimeType("application/json");
+				xhr.open('GET', data_file, true);
+				xhr.onload = function () {
+
+					var res = JSON.parse(this.responseText);
+					self.data_folders = res.folders;
+					self.data_locations = res.locations;
+					self.data_filedata = 'data/' + res.folders[0] + '/' + res.locations[0];
+					self.getItems();
+				}
+				xhr.send();
+				xhr = null;
+		},
+		getItems: function() {
+
+			console.log(this.data_filedata);
+
+			var self = this;
+			var xhr = new XMLHttpRequest();
+				xhr.overrideMimeType("application/json");
+				xhr.open('GET', this.data_filedata, true);
+				xhr.onload = function () {
+
+					self.data_items = JSON.parse(this.responseText);
+
+				}
+				xhr.send();
+				xhr = null;
+		}
+
+	}
+
+});
