0165e1ce37e6889be494646bded5019a249f297f
Jorge Moreno <alterebro@gmail.com>
2016-09-08 17:12:08 +0200

    gulp builder

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp 		= require('gulp'),
+	rename 		= require('gulp-rename'),
+	htmlmin 	= require('gulp-htmlmin'),
+	useref 		= require('gulp-useref'),
+	gulpIf 		= require('gulp-if'),
+	uglify 		= require('gulp-uglify');
+
+gulp.task('build', function() {
+	return 	gulp.src("./_app.html")
+				.pipe(rename("index.html"))
+				.pipe(useref())
+				.pipe(gulpIf('*.js', uglify()))
+				.pipe(gulp.dest("./"));
+});
+gulp.task('compress', function() {
+	return 	gulp.src('index.html', {base: './'})
+			.pipe(htmlmin({ collapseWhitespace: true, removeComments: false }))
+			.pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['build', 'compress']);
